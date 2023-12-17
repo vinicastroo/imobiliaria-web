55f4f8bbdd206c75260cfbeac46d1829cc5f99b5
Vinicius Castro <viniciusc.d.c@hotmail.com>
2023-12-17 16:40:26 -0300

    feat/deploy

--- src/pages/admin/imoveis/criar/index.page.tsx
+++ src/pages/admin/imoveis/criar/index.page.tsx
@@ -262,7 +262,7 @@ export default function CriarImoveis() {
                     onreorderfiles={onFileChange}
                     labelFileTypeNotAllowed="Tipo de arquvio não permitido"
                     files={files}
-                    maxFileSize="5mb"
+                    maxFileSize="3.5mb"
                     labelMaxFileSize="O tamanho maximo toltal dos arquivos permitido é de 5MB"
                     acceptedFileTypes={['image/*']}
                     server={null} // Não usar a opção de servidor interno do FilePond, pois estamos enviando para um backend personalizado
