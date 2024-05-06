cc098005853fb2c408ef8bdba1b8025740e6723d
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-05-06 17:07:30 -0300

    feat/build

---
+++ src/pages/sitemap/index.page.tsx
@@ -0,0 +1,58 @@
+import api from "@/services/api";
+
+const EXTERNAL_DATA_URL = 'https://aurosimobiliaria.com.br';
+
+interface Posts {
+  id:string
+}
+function generateSiteMap(posts:Posts[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+        <loc>${EXTERNAL_DATA_URL}</loc>
+        <lastmod>${new Date()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1</priority>
+     </url>
+     <url>
+        <loc>${EXTERNAL_DATA_URL}/imoveis</loc>
+        <lastmod>${new Date()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1</priority>
+     </url>
+     ${posts
+       .map(({ id }) => {
+         return `
+       <url>
+          <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+          <lastmod>${new Date()}</lastmod>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }:any) {
+  // We make an API call to gather the URLs for our site
+  const response = await api.get('/imovel')
+  const imoveis = response.data.properties
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(imoveis);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
