26e915a666baa99b498ecf7cd6f831898da7e97e
Vinicius Castro <viniciusc.d.c@hotmail.com>
2025-03-10 12:17:55 -0300

    feat/change next config

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
   images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
+    // deviceSizes: [320, 420, 768, 1024, 1200],
     domains: [
       'images.unsplash.com',
       'auros-s3.s3.amazonaws.com',
       'd2wss3tmei5yh1.cloudfront.net',
     ],
-    loader: 'default',
-    unoptimized: false,
+    // loader: 'default',
+    // unoptimized: false,
   },
-  swcMinify: true,
+  // swcMinify: true,
 }
 
 module.exports = nextConfig
